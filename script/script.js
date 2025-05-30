49ecb5503a315319c41519b4bf800169853a9b42
merrickgc <merrickgonzzalo@gmail.com>
2025-05-30 15:06:56 -0600

    Finalizado sin bootstrap

--- script/script.js
+++ script/script.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
-
+    /* Inicio del slide para las categorias*/
     var owl = $('.owl-carousel');
     owl.owlCarousel({
-        items:6,
-        loop:true,
+        items:6, //cantidad de iconos en pantalla
+        loop:true, //reinicio automatico
         margin:10,
         autoplay:false,
-        autoplayTimeout:4500,
+        autoplayTimeout:4500, //duracion transicion
         autoplayHoverPause:true
     });
-   
+   //duración inicio automatico
     owl.trigger('play.owl.autoplay',[4500])
-
 });
+
+//Aletrea de producto agregado
+function carrito(){
+    alert("Producto añadido al carrito");
+}
+    
